893378dad43d4e9408b46e1945e68e1ec4973789
seleniumlearn <seleniumlearn.com@gmail.com>
2018-06-21 13:05:46 +0530

    Demo

---
+++ Demo/src/test/java/Demo/LaunchURL.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class LaunchURL {
+	WebDriver driver;
+	
+  @Test
+  public void printMesg() {
+	  
+	  System.out.println("Hello Maven, Github, Jenkins");
+	
+  }
+  
+ }
